b04414e0adcbff100ee8da43b2d934a8d4896487
Dominique Orban <dominique.orban@gmail.com>
2011-04-13 12:01:32 -0400

    Update to init.

--- tokyo/__init__.py
+++ tokyo/__init__.py
@@ -1,8 +1,8 @@
-from tokyo import *
+import tokyo
 from misc import get_include
 #from _demo_outer import *
 #from _double_speed import *
 #from _single_speed import *
 #from _verify import *
 
-__all__ = filter(lambda s:not s.startswith('_'), dir())
+#__all__ = filter(lambda s:not s.startswith('_'), dir())
